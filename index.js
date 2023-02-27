3245bd02c21308b2f68643e205a05178e25869a7
bfitz83 <115970022+bfitz83@users.noreply.github.com>
2023-02-27 13:51:59 -0500

    js menu added
    still needs styling

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import {menuItems} from "./data.js"
+const menuItemsSection = document.getElementById("menu-items-section")
 
-console.log(menuItems)
+function makeMenu() {
+    menuItems.forEach(function(item){
+        menuItemsSection.innerHTML += `
+           <div class="align">
+                <img scr="${item.image}">
+                    <div class="menu-div">
+                        <p>${item.name}</p>
+                        <p>${item.ingredients}</p>
+                        <p>${item.price}</p>
+                    </div>
+                <button id="add-btn">+</button>
+            </div>        
+        `
+    }
+)}
+
+makeMenu()
