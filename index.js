c7079182c9c382deee98ced4dd3721ceae53b35c
bfitz83 <115970022+bfitz83@users.noreply.github.com>
2023-02-28 21:45:59 -0500

    trying to make the order conformation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import {menuItems} from "./data.js"
 const menuItemsSection = document.getElementById("menu-items-section")
+const orderConformation = document.getElementById("order-conformation")
 
 function makeMenu() {
     menuItems.forEach(function(item){
@@ -9,12 +10,29 @@ function makeMenu() {
                     <div class="menu-div">
                         <p>${item.name}</p>
                         <p>${item.ingredients}</p>
-                        <p>${item.price}</p>
+                        <p data-price="${item.price}">${item.price}</p>
                     </div>
-                <button id="add-btn">+</button>
+                <button id="add-btn" data-food="${item.name}">+</button>
             </div>        
         `
     }
 )}
 
 makeMenu()
+
+// This is doing kinda the right thing. I think that you need to make it so that it just fills in the HTML
+//      with all the options. Maybe use a uuid?
+
+
+document.addEventListener("click", function(e){
+   if(e.target.id === "add-btn"){
+        orderConformation.innerHTML += 
+        `
+            <div>
+                <p>${e.target.dataset.food}</p>
+                <p>${e.target.dataset.price}</p>
+            </div>    
+        `
+        console.log(e.target.dataset.food)
+    }
+})
